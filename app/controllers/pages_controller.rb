7caf3ba6d695a840a1c37dd8da0c0eaec6f4af3e
yspoo <pooyising@gmail.com>
2017-11-01 16:10:10 +0800

    Before Tweet model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,15 @@ class PagesController < ApplicationController
   end
 
   def profile
-    # grab the username from the URL as :id
-    if (User.find_by_username(params[:id]))
-    @username = params[:id]
+# grab the username from the URL as :id
+    if User.find_by_username(params[:id]) # this is to find a User by his/her username(which is unique because of our database validations) using User ID, which is unique by default(database behaviour).
+      @username = params[:id] # this is to change the User ID(which is a number) into a instance variable @username.
+                              # so now whatever we type after /user/ will be overwritten as the NEW ID(which was supposed to be a number previously) will be treated as @username. We can use @username wherever we want in the profile viewpage.
 # using a built-in identifier for grabbing URL parameters. URL paramter :id and since we specified id in the routes as whatever comes after users, rails routing system will know that it is going to look for users. If users exist, whatever after that will be the user id.
     else
       # redirect to 404 (root for now)
       redirect_to root_path, :notice=> "User not found!"
+    end
   end
 
   def explore
