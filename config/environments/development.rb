b73c3e1f0b6c8498b6fe0f53ce52b1b74f34eff3
yspoo <pooyising@gmail.com>
2017-11-06 23:34:17 +0800

    Finished landing page and fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enabled but why still can't get my images from within my application?
+  config.assets.enabled = true
 end
